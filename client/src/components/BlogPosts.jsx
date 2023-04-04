76f2886867102feb9e9d6f79291d0a6bb55c32b3
Camille Williams <camiwills325@gmail.com>
2023-04-04 13:48:47 -0400

    Sort blog posts by date

--- client/src/components/BlogPosts.jsx
+++ client/src/components/BlogPosts.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 
+
 const Posts = () => {
     const [posts, setPosts] = useState([])
     const loadPosts = () => {
@@ -16,16 +17,23 @@ const Posts = () => {
         loadPosts();
     }, []);
 
+    // function to put the blog post titles on screen in order by most recent to oldest
+    posts.sort(function (a, b) {
+        let postA = a.posted;
+        let postB = b.posted;
+        return (postA > postB) ? -1 : (postA < postB) ? 1 : 0;
+    })
+
     return (
         <>
         {posts.map((post) => {
             return (
-                <>
-                <h1>{post.title}</h1>
-                <h4>{post.author}</h4>
+                <li key={post.blog_id}>
+                <h1>{post.title} </h1>
+                <h4>By: {post.author}</h4>
                 <p>{post.content}</p>
                 <h2>{post.highlight1}</h2>
-                </>
+                </li>
             )
         })}
         </>
